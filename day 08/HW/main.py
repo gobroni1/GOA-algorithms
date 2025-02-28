2cf9d16924fe5967ea616bd9a2ffdfb6774119c9
gobroni1 <deathshadow075@gmail.com>
2025-02-28 21:12:48 +0400

    rec +

---
+++ day 08/HW/main.py
@@ -0,0 +1,151 @@
+# ❗მაგალითების ვიზუალიზაცია მოცემულია სურათში❗
+
+## 33
+
+#  A⊂B ⇔ A∩B = A
+
+A = [1,2,3]
+B = [0,1,2,3,4]
+
+# A∩B == A (ყველა ელემენტი A ში არის B ში)
+
+## 34
+
+# A⊂B ⇔ A∪B = B
+
+A = [1,2,3]
+B = [0,1,2,3,4]
+
+# Aს გაერთიანება B სთან იგივე B-ა
+
+## 35
+
+# B⊂A ⇒ (A\B) ∪B = A
+# B ში არის A, ყველა ელემნი რაც A ში არაა და არის B ში + A არის B
+
+## 36
+
+# A∩B = ∅ ⇒(A∪B) \ B = A
+
+A = [1,2,3]
+B = [4,5,6]
+
+# A და B სხვადასხვა სიმრავლეებია, ყველა ელემენტი A+B ში რაც არაა B ში არის A
+
+# ყოველ Aს და Bს დაწერაზე უნდა შევცვალო შიფტი ამიტომ ინგლისურად დავწერ 
+
+## 37
+
+# A is subset of B --> A-C subset of B-C
+
+A = [1, 2, 3]
+B = [1, 2, 3, 4, 5]
+C = [2, 4]
+
+## 38
+
+A = [1, 2, 3]
+B = [1, 2, 3, 4, 5]
+C = [2, 3, 6]
+
+# A is subset of B and C overlaps with part of A and B so C∩A is subset of B
+
+## 39
+
+A = [1, 2, 3]
+B = [1, 2, 3, 4, 5]
+C = [3, 6, 7]
+
+# A is subset of B, A+C is sub set of B+C
+
+## 40
+
+A = [1, 2, 3, 4]
+B = [2, 3, 4, 5]
+C = A-B
+
+# C is in A and not in B, B and C hane no overlap
+
+## 41
+
+A = []
+B = []
+
+# A's and B's overlap is empty, A and B are empty
+
+## 42
+
+A = [1, 2, 3]
+B = [1, 2, 3, 4, 5]
+
+# A-B is empty and all in A is in B
+
+## 43
+
+A = [1, 2, 3]
+B = [4, 5, 6]
+
+# A - B = A and B - A = B so no overlaps
+
+## 44
+
+A = [1, 2, 3]
+B = []
+
+# A + B = A - B and B is empty 
+
+## 45
+
+A = []
+B = [1, 2, 3]
+
+# A's elements that are not in B = A's elements in B and A is empty 
+
+## 46
+
+A = [1, 2]
+B = [3, 4]
+C = [1, 2, 3, 4, 5, 6]
+
+# A + B is subset of C, A and B are in C
+
+## 47
+
+A = [1, 2, 3, 4]
+B = [2, 3, 4, 5]
+C = [2, 3]
+
+# C is sub set of overlap of A and B and must be in A and in B
+
+## 48 
+
+A = [1, 2, 3]
+B = [3, 4]
+C = [1, 2, 4]
+
+# A is subset of C ∪ B, A - B is subset of C
+
+## 49
+
+A = [1, 2, 3]
+B = [1, 2, 3]
+
+# A's overlap with B = A + B and A = B 
+
+## 50 
+
+A = [1, 2]
+B = [1, 2, 3]
+C = [1, 2, 3, 4]
+
+# A is subset of B and B is of C and A + B = B ∩ C
+
+## 51
+
+A = [1, 2, 3]
+X = []
+
+# X is empty
+
+## 52
+
